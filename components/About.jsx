783b8a9b3b85923b9c0b907018357b74d98d005a
Kenny Cen <kennycen1976@gmail.com>
2025-01-25 09:24:08 -0500

    fix style issue in about section

--- components/About.jsx
+++ components/About.jsx
@@ -5,11 +5,14 @@ import { motion } from "motion/react"
 
 const About = ({isDarkMode}) => {
   return (
-    <motion.div id='about' className='w-full px-[12%] py-10 scroll-mt-20'
-    initial={{opacity: 0}}
-    whileInView={{opacity: 1}}
-    transition={{duration: 1}}
+    <motion.div 
+      id='about' 
+      className='w-full py-10 scroll-mt-20 flex justify-center'
+      initial={{opacity: 0}}
+      whileInView={{opacity: 1}}
+      transition={{duration: 1}}
     >
+      <div className='max-w-[1400px] w-[88%]'>
         <motion.h4 
         initial={{opacity: 0, y: -20}}
         whileInView={{opacity: 1, y: 0}}
@@ -47,7 +50,7 @@ const About = ({isDarkMode}) => {
             className='grid -grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
               {infoList.map(({icon, iconDark, title, description}, index)=>(
                 <motion.li 
-                whileInHover={{scale:1.05}}
+                whileHover={{scale:1.05}}
                 className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50' key={index}>
                   <Image src={isDarkMode ? iconDark : icon} alt={title} className='w-7 mt-3' />
                   <h3 className='my-4 font-semibold text-gray-700 dark:text-white'>{title}</h3>
@@ -77,6 +80,7 @@ const About = ({isDarkMode}) => {
             </motion.ul>
           </motion.div>
         </motion.div>
+      </div>
     </motion.div>
   )
 }
